6b2e3a044f223f6228d6d2c0600b6e93ed45c243
Nitwel <nitwel@arcor.de>
2020-03-19 20:02:14 +0100

    added shortcuts to the sidebar

--- src/modules/docs/routes/docs/docs.vue
+++ src/modules/docs/routes/docs/docs.vue
@@ -1,6 +1,21 @@
 <template>
-	<private-view title="Documentation" header-dense :header-dense-auto="false">
-		<iframe id="docs-frame" src="https://docs.directus.io/" frameborder="0"></iframe>
+	<private-view title="Documentation" header-dense :header-dense-auto="true">
+		<iframe id="docs-frame" :src="url" frameBorder="0"></iframe>
+
+		<template #navigation>
+			<v-list nav>
+				<v-list-item
+					v-for="shortcut in shortcuts"
+					:key="shortcut.to"
+					@click="onClick(shortcut.to)"
+				>
+					<v-list-item-icon><v-icon :name="shortcut.icon" /></v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>{{ shortcut.name }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</template>
 	</private-view>
 </template>
 
@@ -8,7 +23,35 @@
 import { defineComponent, computed, ref } from '@vue/composition-api';
 
 export default defineComponent({
-	name: 'docs'
+	name: 'docs',
+	setup(props) {
+		const shortcuts = [
+			{
+				name: 'Introduction',
+				icon: 'info',
+				to: 'https://docs.directus.io/'
+			},
+			{
+				name: 'Getting started',
+				icon: 'flight_takeoff',
+				to: 'https://docs.directus.io/guides/user-guide.html'
+			},
+			{
+				name: 'FAQ',
+				icon: 'help',
+				to:
+					'https://docs.directus.io/getting-started/troubleshooting.html#frequently-asked-questions'
+			}
+		];
+
+		const url = ref('https://docs.directus.io/');
+
+		function onClick(to: string) {
+			url.value = to;
+		}
+
+		return { shortcuts, onClick, url };
+	}
 });
 </script>
 <style lang="scss" scoped>
@@ -17,9 +60,9 @@ export default defineComponent({
 
 	main {
 		#docs-frame {
+			display: block;
 			width: 100%;
 			height: 100%;
-			overflow: hidden;
 		}
 	}
 }
