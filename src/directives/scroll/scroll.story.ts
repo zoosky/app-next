12428cabfbfaeb24d8bb288806bc9b8ec68838e9
Nitwel <nitwel@arcor.de>
2020-03-20 18:02:24 +0100

    added horrizontal events

--- src/directives/scroll/scroll.story.ts
+++ src/directives/scroll/scroll.story.ts
@@ -17,15 +17,32 @@ export default {
 	}
 };
 
-export const withText = () => ({
+export const vertical = () => ({
 	methods: {
 		onScroll: (eventType: ScrollType) => {
+			console.log(eventType.toString());
+
 			action('onScroll', { allowFunction: false });
 		}
 	},
 	template: `
 		<div style="overflow-y: scroll; height: 100%; width: 50%" v-scroll="{callback: onScroll}">
 			<div style="height: 400vh; width: 100%; background: linear-gradient(125deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);"></div>
+        </div>
+	`
+});
+
+export const horrizontal = () => ({
+	methods: {
+		onScroll: (eventType: ScrollType) => {
+			console.log(eventType);
+
+			action('onScroll', { allowFunction: false });
+		}
+	},
+	template: `
+		<div style="overflow-x: scroll; height: 100%; width: 50%" v-scroll="{callback: onScroll}">
+			<div style="height: 100%; width: 400vw; background: linear-gradient(125deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);"></div>
         </div>
 	`
 });
--- src/directives/scroll/scroll.ts
+++ src/directives/scroll/scroll.ts
@@ -8,13 +8,19 @@ type ScrollOptions = {
 	callback: (eventType: ScrollType) => {};
 	thresholdTop: number;
 	thresholdBottom: number;
+	thresholdLeft: number;
+	thresholdRight: number;
 };
 
 enum ScrollType {
-	TOP_ENTER,
-	TOP_LEAVE,
-	BOTTOM_ENTER,
-	BOTTOM_LEAVE
+	TOP_ENTER = 'TOP ENTER',
+	TOP_LEAVE = 'TOP LEAVE',
+	BOTTOM_ENTER = 'BOTTOM ENTER',
+	BOTTOM_LEAVE = 'BOTTOM LEAVE',
+	LEFT_ENTER = 'LEFT ENTER',
+	LEFT_LEAVE = 'LEFT LEAVE',
+	RIGHT_ENTER = 'RIGHT ENTER',
+	RIGHT_LEAVE = 'RIGHT LEAVE'
 }
 
 const Scroll: DirectiveOptions = {
@@ -24,13 +30,47 @@ const Scroll: DirectiveOptions = {
 
 		if (!options.thresholdBottom) options.thresholdBottom = 15;
 		if (!options.thresholdTop) options.thresholdTop = 15;
+		if (!options.thresholdLeft) options.thresholdLeft = 15;
+		if (!options.thresholdRight) options.thresholdRight = 15;
 
 		let reachedTop = true;
 		let reachedBottom = false;
+		let reachedLeft = true;
+		let reachedRight = false;
 
 		element.onscroll = throttle(event => {
-			const { scrollTop, scrollHeight, offsetHeight } = event.target;
+			const {
+				scrollTop,
+				scrollLeft,
+				scrollHeight,
+				scrollWidth,
+				offsetHeight,
+				offsetWidth
+			} = event.target;
 			const maxHeight = scrollHeight - offsetHeight;
+			const maxWidth = scrollWidth - offsetWidth;
+
+			if (scrollLeft <= options.thresholdLeft) {
+				if (!reachedLeft) {
+					options.callback(ScrollType.LEFT_ENTER);
+					reachedLeft = true;
+				}
+			} else if (maxWidth - scrollLeft <= options.thresholdRight) {
+				if (!reachedRight) {
+					options.callback(ScrollType.RIGHT_ENTER);
+					reachedRight = true;
+				}
+			} else {
+				if (reachedLeft) {
+					options.callback(ScrollType.LEFT_LEAVE);
+				}
+				if (reachedRight) {
+					options.callback(ScrollType.RIGHT_LEAVE);
+				}
+
+				reachedLeft = false;
+				reachedRight = false;
+			}
 
 			if (scrollTop <= options.thresholdTop) {
 				if (!reachedTop) {
