0e912791cc015c4a5eba84ea7361f1b8ee5f7216
Jacob Rienstra <jacobdrienstra@gmail.com>
2020-04-12 23:41:37 -0400

    added toggle define at interface root

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,6 @@
 import InterfaceTextInput from './text-input/';
 import InterfaceSwitch from './switch/';
+import InterfaceToggle from './toggle/';
 
-export const interfaces = [InterfaceTextInput, InterfaceSwitch];
+export const interfaces = [InterfaceTextInput, InterfaceSwitch, InterfaceToggle];
 export default interfaces;
