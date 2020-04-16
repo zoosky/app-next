c5c1353c0c76385bdef83231c4af8e414e68d8b9
rijkvanzanten <rijkvanzanten@me.com>
2020-04-16 17:08:38 -0400

    Start on cards layout

---
+++ src/layouts/cards/cards.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="cards-layout">
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
