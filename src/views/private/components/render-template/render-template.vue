173cdae4b84d5d666e723a2d46a8d07553ddaf29
rijkvanzanten <rijkvanzanten@me.com>
2020-04-17 16:56:34 -0400

    Make render template reactive

--- src/views/private/components/render-template/render-template.vue
+++ src/views/private/components/render-template/render-template.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from '@vue/composition-api';
+import { defineComponent, PropType, computed } from '@vue/composition-api';
 import useFieldsStore from '@/stores/fields';
 import { get } from 'lodash';
 import { Field } from '@/stores/fields/types';
@@ -41,42 +41,50 @@ export default defineComponent({
 
 		const regex = /({{.*?}})/g;
 
-		const parts = props.template.split(regex).map((part) => {
-			if (part.startsWith('{{') === false) return part;
+		const parts = computed(() =>
+			props.template.split(regex).map((part) => {
+				if (part.startsWith('{{') === false) return part;
 
-			const fieldKey = part.replace(/{{/g, '').replace(/}}/g, '').trim();
-			const field: Field | null = fieldsStore.getField(props.collection, fieldKey);
+				const fieldKey = part.replace(/{{/g, '').replace(/}}/g, '').trim();
+				const field: Field | null = fieldsStore.getField(props.collection, fieldKey);
 
-			// Instead of crashing when the field doesn't exist, we'll render a couple question
-			// marks to indicate it's absence
-			if (!field) return '???';
+				// Instead of crashing when the field doesn't exist, we'll render a couple question
+				// marks to indicate it's absence
+				if (!field) return '???';
 
-			// Try getting the value from the item, return some question marks if it doesn't exist
-			const value = get(props.item, fieldKey);
-			if (value === undefined) return '???';
+				// Try getting the value from the item, return some question marks if it doesn't exist
+				const value = get(props.item, fieldKey);
+				if (value === undefined) return '???';
 
-			// If no display is configured, we can render the raw value
-			if (field.display === null) return value;
+				// If no display is configured, we can render the raw value
+				if (field.display === null) return value;
 
-			const displayInfo = displays.find((display) => display.id === field.display);
+				const displayInfo = displays.find((display) => display.id === field.display);
 
-			// If used display doesn't exist in the current project, return raw value
-			if (!displayInfo) return value;
+				// If used display doesn't exist in the current project, return raw value
+				if (!displayInfo) return value;
 
-			// If the display handler is a function, we parse the value and return the result
-			if (typeof displayInfo.handler === 'function') {
-				const handler = displayInfo.handler as Function;
-				return handler(value, field.display_options);
-			}
+				// If the display handler is a function, we parse the value and return the result
+				if (typeof displayInfo.handler === 'function') {
+					const handler = displayInfo.handler as Function;
+					return handler(value, field.display_options);
+				}
 
-			return {
-				component: field.display,
-				options: field.display_options,
-				value: value,
-			};
-		});
+				return {
+					component: field.display,
+					options: field.display_options,
+					value: value,
+				};
+			})
+		);
 
 		return { parts };
 	},
 });
 </script>
+
+<style lang="scss" scoped>
+.render-template {
+	display: contents;
+}
+</style>
